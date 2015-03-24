74dea8c9c510c2bf1d0443aa11649e6548aeee13
AndriyMand <mandybur.andriy@gmail.com>
2015-03-25 00:42:33 +0200

    WSA

--- ShortestPathLab/Main.cs
+++ ShortestPathLab/Main.cs
@@ -11,18 +11,6 @@ namespace LabiryntSpace
         public static void Main(string[] args)
         {
 
-            FileStream fin;
-            try
-            {
-                fin = new FileStream(args[0], FileMode.Open);
-            }
-            catch (IOException exc)
-            {
-                Console.WriteLine("Помилка! Не вдається вiдкрити файл!");
-                Console.WriteLine(exc.Message);
-                return;
-            }
-
             Checking lab = new Checking(args[0]);
 
             //Checking lab = new Checking("data.txt");
